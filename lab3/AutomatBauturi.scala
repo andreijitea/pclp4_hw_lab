fce67b3726a51f461410d6ec3a54a6b44d3d5228
Andrei <andrei_petru.jitea@stud.acs.upb.ro>
2025-01-08 15:32:21 +0200

    Rezolvat problema lab3

--- lab3/AutomatBauturi.scala
+++ lab3/AutomatBauturi.scala
@@ -1,137 +1,165 @@
 import chisel3._
 import chisel3.util._
 
+class AutomatBauturiIO extends Bundle {
+  // intrari
+  val clk            = Input(Clock())
+  val reset          = Input(Bool())
+  
+  val leu1           = Input(Bool())
+  val lei5           = Input(Bool())
+  val lei10          = Input(Bool())
+
+  val cerere_produs  = Input(UInt(4.W)) // 0..10
+  val cerere_rest    = Input(Bool())
+
+  // iesiri
+  val produs         = Output(UInt(4.W)) // 0..10
+  val leu1_rest      = Output(Bool())
+  val lei5_rest      = Output(Bool())
+  val refuza_bani    = Output(Bool())
+}
+
 class AutomatBauturi extends Module {
-  val io = IO(new Bundle {
-    val reset_input    = Input(Bool())
-    val leu1           = Input(Bool())
-    val lei5           = Input(Bool())
-    val lei10          = Input(Bool())
-    val cerere_produs  = Input(UInt(4.W))
-    val cerere_rest    = Input(Bool())
+  val io = IO(new AutomatBauturiIO)
 
-    val produs         = Output(UInt(4.W))
-    val leu1_rest      = Output(Bool())
-    val lei5_rest      = Output(Bool())
-    val refuza_bani    = Output(Bool())
-  })
+  // definire stari
+  val sStart :: sAccBani  :: sDaProdus :: sDaRest :: Nil = Enum(4)
 
-  val start :: acc_bani :: da_produs :: da_rest :: Nil = Enum(4)
-  val stare = RegInit(start)
 
-  val total_bani    = RegInit(0.U(4.W))
-  val cereri_produs = RegInit(0.U(2.W))
-  val cost_mem      = RegInit(0.U(4.W))
+  val stateReg           = RegInit(sStart)
+  val totalBaniReg       = RegInit(0.U(4.W))
+  val cereriProdusReg    = RegInit(0.U(2.W))
+  val costMemReg         = RegInit(0.U(4.W))
 
-  val produs_reg      = RegInit(0.U(4.W))
-  val leu1_rest_reg   = RegInit(false.B)
-  val lei5_rest_reg   = RegInit(false.B)
-  val refuza_bani_reg = RegInit(false.B)
+  val produsReg          = RegInit(0.U(4.W))
+  val leu1RestReg        = RegInit(false.B)
+  val lei5RestReg        = RegInit(false.B)
+  val refuzaBaniReg      = RegInit(false.B)
 
-  io.produs      := produs_reg
-  io.leu1_rest   := leu1_rest_reg
-  io.lei5_rest   := lei5_rest_reg
-  io.refuza_bani := refuza_bani_reg
+  io.produs      := produsReg
+  io.leu1_rest   := leu1RestReg
+  io.lei5_rest   := lei5RestReg
+  io.refuza_bani := refuzaBaniReg
 
-  when (io.reset_input) {
-    stare          := start
-    total_bani     := 0.U
-    cereri_produs  := 0.U
-    cost_mem       := 0.U
+  val nextState         = WireDefault(stateReg)
+  val nextTotalBani     = WireDefault(totalBaniReg)
+  val nextCereriProdus  = WireDefault(cereriProdusReg)
+  val nextCostMem       = WireDefault(costMemReg)
 
-    produs_reg     := 0.U
-    leu1_rest_reg  := false.B
-    lei5_rest_reg  := false.B
-    refuza_bani_reg:= false.B
-  } .otherwise {
-    produs_reg      := 0.U
-    leu1_rest_reg   := false.B
-    lei5_rest_reg   := false.B
-    refuza_bani_reg := false.B
+  val nextProdusReg     = WireDefault(0.U(4.W))
+  val nextLeu1RestReg   = WireDefault(false.B)
+  val nextLei5RestReg   = WireDefault(false.B)
+  val nextRefuzaBaniReg = WireDefault(false.B)
 
-    switch (stare) {
-      is (start) {
-        total_bani    := 0.U
-        cost_mem      := 0.U
 
-        when (io.leu1) {
-          total_bani := 1.U
-          stare      := acc_bani
-        } .elsewhen (io.lei5) {
-          total_bani := 5.U
-          stare      := acc_bani
-        } .elsewhen (io.lei10) {
-          total_bani := 10.U
-          stare      := acc_bani
-        } .otherwise {
-          stare := start
+  switch(stateReg) {
+    is(sStart) {
+      // reseteaza iesirile
+      nextTotalBani    := 0.U
+      nextCostMem      := 0.U
+      nextCereriProdus := 0.U
+
+      when(io.leu1) {
+        nextTotalBani := 1.U
+        nextState     := sAccBani
+      }.elsewhen(io.lei5) {
+        nextTotalBani := 5.U
+        nextState     := sAccBani
+      }.elsewhen(io.lei10) {
+        nextTotalBani := 10.U
+        nextState     := sAccBani
+      }.otherwise {
+        nextState     := sStart
+      }
+    }
+
+    is(sAccBani) {
+      // reseteaza iesirile
+      nextProdusReg      := 0.U
+      nextLeu1RestReg    := false.B
+      nextLei5RestReg    := false.B
+      nextRefuzaBaniReg  := false.B
+
+      // primeste bancnote
+      when(io.leu1) {
+        when(totalBaniReg + 1.U > 10.U) {
+          nextRefuzaBaniReg := true.B
+        }.otherwise {
+          nextTotalBani := totalBaniReg + 1.U
         }
+      }.elsewhen(io.lei5) {
+        when(totalBaniReg + 5.U > 10.U) {
+          nextRefuzaBaniReg := true.B
+        }.otherwise {
+          nextTotalBani := totalBaniReg + 5.U
+        }
+      }.elsewhen(io.lei10) {
+        nextRefuzaBaniReg := true.B
       }
 
-      is (acc_bani) {
-        when (io.leu1) {
-          when (total_bani < 10.U) {
-            when ((total_bani + 1.U) > 10.U) {
-              refuza_bani_reg := true.B
-            } .otherwise {
-              total_bani := total_bani + 1.U
-            }
-          }
-        } .elsewhen (io.lei5) {
-          when (total_bani < 10.U) {
-            when ((total_bani + 5.U) > 10.U) {
-              refuza_bani_reg := true.B
-            } .otherwise {
-              total_bani := total_bani + 5.U
-            }
-          }
-        } .elsewhen (io.lei10) {
-          refuza_bani_reg := true.B
+      // cerere de produs
+      when(io.cerere_produs > 0.U) {
+        when(totalBaniReg >= io.cerere_produs) {
+          nextCostMem  := io.cerere_produs
+          nextState    := sDaProdus
         }
-
-        when (io.cerere_produs > 0.U) {
-          when (total_bani >= io.cerere_produs) {
-            cost_mem := io.cerere_produs
-            stare    := da_produs
-          } .otherwise {
-            stare := acc_bani
-          }
-        } .elsewhen (io.cerere_rest) {
-          when (total_bani > 0.U) {
-            stare := da_rest
-          } .otherwise {
-            stare := start
-          }
-        } .otherwise {
-          stare := acc_bani
-        }
-      }
-
-      is (da_produs) {
-        cereri_produs := cereri_produs + 1.U
-
-        when (cereri_produs >= 2.U) {
-          stare := da_rest
-        } .otherwise {
-          produs_reg   := cost_mem
-          total_bani   := total_bani - cost_mem
-          stare        := acc_bani
-        }
-      }
-
-      is (da_rest) {
-        when (total_bani >= 5.U) {
-          lei5_rest_reg  := true.B
-          total_bani     := total_bani - 5.U
-          stare          := da_rest
-        } .elsewhen (total_bani > 0.U) {
-          leu1_rest_reg  := true.B
-          total_bani     := total_bani - 1.U
-          stare          := da_rest
-        } .otherwise {
-          stare := start
+      }.elsewhen(io.cerere_rest) {
+        when(totalBaniReg > 0.U) {
+          nextState := sDaRest
+        }.otherwise {
+          nextState := sStart
         }
       }
     }
+
+    is(sDaProdus) {
+      nextCereriProdus := cereriProdusReg + 1.U
+      
+      nextProdusReg    := 0.U
+      nextLeu1RestReg  := false.B
+      nextLei5RestReg  := false.B
+      nextRefuzaBaniReg:= false.B
+
+      when(cereriProdusReg >= 2.U) {
+        nextState := sDaRest
+      }.otherwise {
+        nextProdusReg  := costMemReg
+        nextTotalBani  := totalBaniReg - costMemReg
+        nextState      := sAccBani
+      }
+    }
+
+    is(sDaRest) {
+      nextProdusReg     := 0.U
+      nextRefuzaBaniReg := false.B
+
+      when(totalBaniReg >= 5.U) {
+        nextLei5RestReg := true.B
+        nextLeu1RestReg := false.B
+        nextTotalBani   := totalBaniReg - 5.U
+        nextState       := sDaRest
+      }.elsewhen(totalBaniReg > 0.U) {
+        nextLei5RestReg := false.B
+        nextLeu1RestReg := true.B
+        nextTotalBani   := totalBaniReg - 1.U
+        nextState       := sDaRest
+      }.otherwise {
+        nextLei5RestReg := false.B
+        nextLeu1RestReg := false.B
+        nextState       := sStart
+      }
+    }
   }
+
+  // actualizare registri
+  stateReg        := nextState
+  totalBaniReg    := nextTotalBani
+  cereriProdusReg := nextCereriProdus
+  costMemReg      := nextCostMem
+
+  produsReg       := nextProdusReg
+  leu1RestReg     := nextLeu1RestReg
+  lei5RestReg     := nextLei5RestReg
+  refuzaBaniReg   := nextRefuzaBaniReg
 }
